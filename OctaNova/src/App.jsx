8d210d41e59e4e7fa2c692ce7993941e16dc9988
Chigozirim victor <chigozirimvictorasogwa@gmail.com>
2025-02-20 11:27:03 -0800

    Main

---
+++ OctaNova/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+
+ const App = () => {
+  
+
+  return (
+    <>
+      <div>
+      <h1 className="text-3xl font-bold underline decoration-red-600 ">
+      Hello world!
+       </h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
